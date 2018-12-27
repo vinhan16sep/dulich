3477d2e7fbdc5a23bcc1a5568b65e9a3b7498ba7
vinhan16sep <vinhan16sep@gmail.com>
2018-12-28 01:00:00 +0700

    update

--- application/language/vietnamese/vietnamese_lang.php
+++ application/language/vietnamese/vietnamese_lang.php
@@ -8,6 +8,19 @@
 $lang['detector'] = 'vi';
 $lang['ofvietnam'] = 'của Việt Nam';
 
+// Homepage text
+$lang['experience_activities'] = 'Trải nghiệm các hoạt động';
+$lang['festivals_events'] = 'Lễ hội & Sự kiện';
+$lang['blog_review'] = 'Góc chia sẻ';
+$lang['see_all_destination'] = 'Xem toàn bộ';
+$lang['view_detail'] = 'Xem chi tiết';
+$lang['more_content'] = 'Xem toàn bộ';
+
+// Region
+$lang['north'] = 'Miền Bắc';
+$lang['middle'] = 'Miền Trung';
+$lang['south'] = 'Miền Nam';
+
 // Menu
 $lang['menu_destination'] = 'Điểm đến';
 $lang['menu_event'] = 'Sự kiện';
